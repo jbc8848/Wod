7dd151f18a09400d274aec12277256910ec7f564
jbc8848 <jbc.guide.ch@gmail.com>
2026-08-08 10:18:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Opportuniste — service worker. Incrémente CACHE à chaque mise à jour de l'app. */
-const CACHE = 'oppo-v11';
+const CACHE = 'oppo-v14';
 const ASSETS = [
   './',
   './index.html',
